9a9f50a75323f47e8436b1cbd338483fd5433d39
Arch2 <new@mail.ru>
2020-01-04 01:44:49 +0300

    wagtails base

--- panda/home/models.py
+++ panda/home/models.py
@@ -1,8 +1,37 @@
 from django.db import models
 
 from wagtail.core.models import Page
-
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 class HomePage(Page):
     templates = 'home/home_page.html'
+    max_count = 1
     
+    company_info = RichTextField(features=['bold','italic'])
+    some_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+'
+
+    )
+
+    #some link to another page
+    some_link = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('company_info'),
+        ImageChooserPanel('some_image'),
+        PageChooserPanel('some_link')
+        
+    ]
