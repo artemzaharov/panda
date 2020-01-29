0fa6f0dc5db45b50ffd8199309aa601bf6794f30
arch <mycode.dealer@gmail.com>
2020-01-29 04:28:22 +0300

    menu+text

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/arch/20panda/panda/env"
+set -gx VIRTUAL_ENV "/home/arch/Рабочий стол/mandamenu/panda/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
